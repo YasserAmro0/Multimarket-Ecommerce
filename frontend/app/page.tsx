380eafbeeebbe68f6e0874759ee0c6907ceac73f
Yasser Alaa <87650778+YasserAmro0@users.noreply.github.com>
2023-08-22 13:31:11 +0300

    add navbar component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,10 @@
 
-const Home = () => (<div> Hi</div>);
-  
+const Home = () => {
+    return (
+        <div
+        > Hi</div>
+    );
+}
+
 export default Home;
 
---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import Link from 'next/link'
+import Image from 'next/image';
+import logoImage from '../app/assets/images/eco-logo.png';
+
+const Navbar = () =>
+     (
+    <div className='bg-gray-50  shadow-md'>
+        <div className='container flex justify-between'>
+            {/* Logo */}
+            <div className='flex gap-2'>
+                <Image
+                    src={logoImage}
+                    width={24}
+                    height={24}
+                    alt='logo'
+                />
+                <h3 className='text-xl font-bold'>
+                    Multimarkt
+                </h3>
+            </div>
+
+            {/* links  */}
+            <div className='flex justify-between'>
+                <ul className="flex space-x-8">
+                    <li>
+                        <Link href='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link href='/shop'>Shop</Link>
+                    </li>
+                    <li>
+                        <Link href='/cart'>Cart</Link>
+                    </li>
+                </ul>
+            </div>
+
+
+            {/* Icons */}
+            <div className='flex justify-between space-x-4'>
+                <i className="ri-heart-line text-xl cursor-pointer"></i>
+                <i className="ri-shopping-cart-line text-xl cursor-pointer"></i>
+                <i className="ri-user-fill text-xl cursor-pointer"></i>
+            </div>
+            
+           
+        </div>
+    
+    </div>
+);
+
+export default Navbar;
