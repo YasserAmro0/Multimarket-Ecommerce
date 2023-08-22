f5a0314a51a45238a3e3b1311b14cd2ec9a27ec5
Yasser Alaa <87650778+YasserAmro0@users.noreply.github.com>
2023-08-22 16:33:08 +0300

    build add menuDropdown

--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -1,40 +1,46 @@
 import React from 'react';
 import Image from 'next/image';
 import HeroImage from '../app/assets/images/hero-img.png';
+import Link from 'next/link';
 const Hero = () => {
-  return (
-      <div className='bg-blue-200 h-full pt-24 pb-6'>
-          <div className='container flex '>
-              {/* text about hero section */}
-              <div>
-                  <p className='pb-6'>trending Product in 2023</p>
-                  <h2 className='pb-4 font-bold'>
-                      Make your Interior More ,<br/>
-                      Minimalistic & Modern
-                  </h2>
-                  <p className='pb-10'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor maiores aperiam temporibus vero
-                      dolorum beatae perferendis incidunt soluta placeat harum eius. Est, ducimus praesentium.
-                  </p>
-                  <button
-                      className='bg-[#0A1D37] hover:bg-blue-900 text-white font-bold py-2 px-4 rounded'
-                  > SHOP NOW</button>
-              </div>
+    return (
+        <div className='bg-blue-200 h-full pt-24 pb-6'>
+            <div className='container flex '>
+                {/* text about hero section */}
+                <div>
+                    <p className='pb-6'>trending Product in 2023</p>
+                    <h2 className='pb-4 font-bold'>
+                        Make your Interior More ,<br />
+                        Minimalistic & Modern
+                    </h2>
+                    <p className='pb-10'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor maiores aperiam temporibus vero
+                        dolorum beatae perferendis incidunt soluta placeat harum eius. Est, ducimus praesentium.
+                    </p>
+                    <Link href='/shop'>
+                        <button
+                            className='bg-[#0A1D37] hover:bg-blue-900 text-white font-bold py-2 px-4 rounded'
+                        >
 
-              {/* Image */}
-              <div>
-                  <Image
-                      src={HeroImage}
-                      width={900}
-                      height={900}
-                      alt='hero'
-                      
-                  />
-                  
-              </div>
-          </div>
-          
-    </div>
-  )
+                            SHOP NOW
+                        </button>
+                    </Link>
+                </div>
+
+                {/* Image */}
+                <div>
+                    <Image
+                        src={HeroImage}
+                        width={900}
+                        height={900}
+                        alt='hero'
+
+                    />
+
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default Hero
--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -3,51 +3,74 @@
 import Link from 'next/link'
 import Image from 'next/image';
 import logoImage from '../app/assets/images/eco-logo.png';
+import { useState } from 'react';
 
-const Navbar = () =>
-     (
-    <div className='bg-gray-50  shadow-md'>
-        <div className='container flex justify-between'>
-            {/* Logo */}
-            <div className='flex gap-2'>
-                <Image
-                    src={logoImage}
-                    width={24}
-                    height={24}
-                    alt='logo'
-                />
-                <h3 className='text-xl font-bold'>
-                    Multimarkt
-                </h3>
+const Navbar = () => {
+    const [isExpanded, toggleExpansion] = useState(false);
+    const toggleMenu = () => {
+        toggleExpansion(!isExpanded);
+    };
+    return (
+        <div className='bg-gray-50  shadow-md'>
+            <div className='container flex justify-between'>
+                {/* Logo */}
+                <div className='flex gap-2'>
+                    <Image
+                        src={logoImage}
+                        width={24}
+                        height={24}
+                        alt='logo'
+                    />
+                    <h3 className='text-xl font-bold'>
+                        Multimarkt
+                    </h3>
+                </div>
+
+                {/* links  */}
+                <div className='flex justify-between'>
+                    <ul className="flex space-x-8">
+                        <li>
+                            <Link href='/'>Home</Link>
+                        </li>
+                        <li>
+                            <Link href='/shop'>Shop</Link>
+                        </li>
+                        <li>
+                            <Link href='/cart'>Cart</Link>
+                        </li>
+                    </ul>
+                </div>
+
+
+                {/* Icons */}
+                <div className='flex justify-between space-x-4'>
+                    <i className="ri-heart-line text-xl cursor-pointer"></i>
+                    <i className="ri-shopping-cart-line text-xl cursor-pointer"></i>
+                    <i className="ri-menu-line text-xl cursor-pointer" onClick={toggleMenu}></i>
+                    <div id="menuDropdown" className={` absolute ${isExpanded ? '' : 'hidden' } bg-gray-50 p-4 rounded shadow-md right-20 top-12`}>
+                        <ul className='px-4'>
+                            <li>
+                                <Link href='/login'>
+                                    Login
+                                </Link>
+                            </li>
+                            <li className='border-t-2 w-full'>
+                                <Link href='/signup'>
+                                    SignUp
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+
+
             </div>
 
-            {/* links  */}
-            <div className='flex justify-between'>
-                <ul className="flex space-x-8">
-                    <li>
-                        <Link href='/'>Home</Link>
-                    </li>
-                    <li>
-                        <Link href='/shop'>Shop</Link>
-                    </li>
-                    <li>
-                        <Link href='/cart'>Cart</Link>
-                    </li>
-                </ul>
-            </div>
-
-
-            {/* Icons */}
-            <div className='flex justify-between space-x-4'>
-                <i className="ri-heart-line text-xl cursor-pointer"></i>
-                <i className="ri-shopping-cart-line text-xl cursor-pointer"></i>
-                <i className="ri-menu-line text-xl cursor-pointer"></i>
-            </div>
-            
-           
         </div>
+    );
     
-    </div>
-);
+}
+    
 
 export default Navbar;
