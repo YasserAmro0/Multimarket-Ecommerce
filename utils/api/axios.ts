b06882c483a396334834f8849dd67d209b9474b9
Yasser Alaa <87650778+YasserAmro0@users.noreply.github.com>
2023-09-26 21:43:22 +0300

    add url backend

--- utils/api/axios.ts
+++ utils/api/axios.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
-
 const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
+    baseURL: process.env.SERVER_URL,
 });
 
 axiosInstance.interceptors.request.use(
